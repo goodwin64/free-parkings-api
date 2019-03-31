a09d7e83f0f534b3ce4ec0f2f14fb7b8e6c5075f
Max Donchenko <Maksym.Donchenko@cloudmade.com>
2019-03-31 18:26:00 +0300

    cors + default view

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
 var parkingsRouter = require('./routes/parkings');
 var areaRouter = require('./routes/areaRouter');
 
@@ -19,6 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/parkings', parkingsRouter);
