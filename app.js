ee8fc02751362da54d144d0b3441ad1c0390d50d
Max Donchenko <Maksym.Donchenko@cloudmade.com>
2019-04-06 11:21:25 +0300

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes');
 const parkingsRouter = require('./routes/parkings');
@@ -20,13 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/parkings', parkingsRouter);
